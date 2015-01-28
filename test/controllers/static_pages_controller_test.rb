74232393c87122b903e0ac1f631b9ebf93cdc00e
Allen Wang <allenwang@utexas.edu>
2015-01-28 00:17:27 -0600

    Added a users controller and updated routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | The Roommate App!"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | The Roommate App!"
+  end
 end
