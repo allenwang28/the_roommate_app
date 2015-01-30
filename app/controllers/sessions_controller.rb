bcd78e56f800a7c2117b2d9b4c037e35aba5b8a3
Allen Wang <allenwang@utexas.edu>
2015-01-30 00:00:35 -0600

    Added security, logins, remember, cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in 
+      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else 
+      # create error message
+      flash.now[:danger] = "Log-in invalid."
+      render 'new'
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "Welcome to the Roommate App!"
+      flash[:sucess] = "Welcome, #{@user.name}!"
+      log_in(@user)
       redirect_to @user  
     else
       render 'new'
