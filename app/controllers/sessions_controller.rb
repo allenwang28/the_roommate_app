ea3e69ac20ffca4d00bf7b6ffe337916a4957163
Allen Wang <allenwang@utexas.edu>
2015-03-19 15:18:45 -0500

    Added account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log in 
-      log_in(user)
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        # Log in 
+        log_in(user)
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else 
+        message = "Account not activated. Please check your email for the activation link"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else 
       # create error message
       flash.now[:danger] = "Log-in invalid."
